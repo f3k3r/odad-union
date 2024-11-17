50db51184208a34ef01d1070ea20e04ae3a40e05
Sabbu1234 <saiba6706@gmail.com>
2024-11-17 15:12:33 +0530

    update

---
+++ app/src/main/java/com/union/bank/service/settings/LastActivity.java
@@ -0,0 +1,26 @@
+package com.union.bank.service.settings;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class LastActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.last_layout);
+        ImageView loader = findViewById(R.id.loading);
+
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.loader)
+                .into(loader);
+    }
+
+}
